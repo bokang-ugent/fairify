077fbeba44488a34cd12cff6676a45b7219cc84e
Bo Kang <klinux@live.com>
2021-02-26 08:15:13 +0100

    upload files for computation.

--- fairify/app.py
+++ fairify/app.py
@@ -4,14 +4,20 @@ from flask import Flask
 from flask_restful import Api
 
 
-from resources.fairify import BinaryLabelDatasetMetric
+from resources.fairify import BinaryLabelDatasetMetric, RepresentationMetric
 
 
 BINARY_LABEL_METRIC_API = os.environ["BINARY_LABEL_METRIC_API"]
+REPRESENTATION_METRIC_API = os.environ["REPRESENTATION_METRIC_API"]
+
 
 app = Flask(__name__)
 api = Api(app)
+app.config["UPLOAD_FOLDER"] = os.environ["UPLOAD_FOLDER"]
+app.secret_key = "super secret key"
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
-
-kwargs = {}
+kwargs = {"ALLOWED_EXTENSIONS": os.environ["ALLOWED_EXTENSIONS"]}
 api.add_resource(BinaryLabelDatasetMetric, BINARY_LABEL_METRIC_API, resource_class_kwargs=kwargs)
+api.add_resource(RepresentationMetric, REPRESENTATION_METRIC_API, resource_class_kwargs=kwargs)
--- fairify/resources/fairify.py
+++ fairify/resources/fairify.py
@@ -1,7 +1,10 @@
 import json
+import os
+
 import numpy as np
-from flask import jsonify, request
+from flask import flash, jsonify, request, redirect, url_for
 from flask_restful import Resource, reqparse
+from werkzeug.utils import secure_filename
 
 class BinaryLabelDatasetMetric(Resource):
     def demographic_parity(self, target, pred, group):
@@ -29,7 +32,46 @@ class BinaryLabelDatasetMetric(Resource):
         else:            
             metrics = ["demographic_parity", "equalized_opportunity", "acceptance_rate_parity"]
         return jsonify({metric: self.get_metrics_func(metric)(target, pred, group) for metric in metrics})
-        
+
+class RepresentationMetric(Resource):
+    def __init__(self, **kwargs):
+        self.ALLOWED_EXTENSIONS = kwargs.get("ALLOWED_EXTENSIONS")
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument("uid", type=str)
+
+    def allowed_file(self, filename, ALLOWED_EXTENSIONS):
+        return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    def post(self):
+        # check if the post request has the file part        
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename                
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        print(request.files, flush=True)
+        if file and self.allowed_file(file.filename, self.ALLOWED_EXTENSIONS):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            data = pickle.load(open(filepath, "rb"))
+            print(data.keys(), flush=True)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+        return '''
+            <!doctype html>
+            <title>Upload new File</title>
+            <h1>Upload new File</h1>
+            <form method=post enctype=multipart/form-data>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+            </form>
+            '''
 
 class RecommendationMetric(Resource):
     def __init__(self, **kwargs):
